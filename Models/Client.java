f7e87a8cd6e6f3a750c39c70941aba3d5203d8d8
HenryCortez <100394133+HenryCortez@users.noreply.github.com>
2023-05-21 18:12:05 -0500

    Client Dao

--- Models/Client.java
+++ Models/Client.java
@@ -1,68 +1,88 @@
 package Models;
 
 public class Client {
-    private String fullname;
+    private String names;
+    private String lastnames;
     private String id;
     private String typeId;
     private String address;
     private String phoneNumber;
     private String email;
 
-    
-
-    public Client(String fullname, String id, String typeId, String address, String phoneNumber, String email) {
-        this.fullname = fullname;
+    public Client(String names, String lastnames, String id, String typeId, String address, String phoneNumber,
+            String email) {
+        this.names = names;
+        this.lastnames = lastnames;
         this.id = id;
         this.typeId = typeId;
         this.address = address;
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
-    
-    public String getFullname() {
-        return fullname;
+
+    public String getNames() {
+        return names;
     }
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
+
+    public void setNames(String names) {
+        this.names = names;
     }
+
+    public String getLastnames() {
+        return lastnames;
+    }
+
+    public void setLastnames(String lastnames) {
+        this.lastnames = lastnames;
+    }
+
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public String getTypeId() {
         return typeId;
     }
+
     public void setTypeId(String typeId) {
         this.typeId = typeId;
     }
+
     public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
+
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
 
-    
-
-    /*public static void main(String[] args) {
-        Validations test = new Validations();
-        if (test.validateID("1805472387"))
-        System.out.println("si es");
-        else
-        System.out.println("no es");
-    }*/
+    /*
+     * public static void main(String[] args) {
+     * Validations test = new Validations();
+     * if (test.validateID("1805472387"))
+     * System.out.println("si es");
+     * else
+     * System.out.println("no es");
+     * }
+     */
 }
--- Models/ClientDao.java
+++ Models/ClientDao.java
@@ -1,5 +1,48 @@
 package Models;
 
+import java.util.LinkedList;
+import java.util.List;
+import Complements.Messages;
+import Complements.ScannerReader;
+
 public class ClientDao {
+    List<Client> clients = new LinkedList<>();
+
+    public boolean insertClient(String names, String lastnames, String id, String typeId, String address, String phoneNumber, String email) {
+        try {
+            clients.add(new Client(names,lastnames, id, typeId, address, phoneNumber, email));
+            return true;
+        } catch (Exception e) {
+            Messages.errorMessage();
+        }
+        return false;
+    }
+
+    public boolean updateProductClient(String id){
+        try {
+            for (Client client : clients) {
+                if (client.getId().equals(id)) {
+                    int option = ScannerReader.readUpdateProductOption();
+                    switch (option) {
+                        case 1:
+                            String newName = ScannerReader.readProductName();
+                            pro.setName(newName);
+                            break;
+                        case 2:
+                            double newPrice = ScannerReader.readProductPriceByUnit();
+                            pro.setPriceByUnit(newPrice);
+                            break;
+                        default:
+                            Messages.switchDefaultMessage();
+                            break;
+                    }
+                }
+            }
+            return true;
+        } catch (NullPointerException ex) {
+            Messages.errorMessage();
+            return false;
+        }
+    }
 
 }
