95b754d96e2fee92ce37b26ddd7a5236c17a5cef
Matias Tite <matiastite1@gmail.com>
2023-05-21 16:17:12 -0500

    Codigo Scanner,Product y ProductDao, con esto ya esta igual al otro proyecto

--- Complements/ScannerReader.java
+++ Complements/ScannerReader.java
@@ -1,5 +1,29 @@
 package Complements;
 
-public class ScannerReader {
+import java.util.Scanner;
 
+public class ScannerReader {
+    private static final Scanner SC = new Scanner(System.in);
+
+    // Aqui ejemplo de como uso el metodo del mensaje de pedir nombre y en esta
+    // clase Scanner lee el nombre ingresado por teclado
+    public static String readName() {
+        Messages.requestName();
+        return SC.nextLine();
+    }
+
+    public static int readUpdateProductOption() {
+        Messages.updateProductOptions();
+        return SC.nextInt();
+    }
+
+    public static String readProductName() {
+        Messages.requestProductName();
+        return SC.nextLine();
+    }
+
+    public static double readProductPriceByUnit() {
+        Messages.requestProductPriceByUnit();
+        return SC.nextDouble();
+    }
 }
