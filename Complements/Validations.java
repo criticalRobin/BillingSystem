bdd024e09f156ecb6bcf86ee1cfd08fe1d472957
Davayme <dayme1937@uta.edu.ec>
2023-05-21 16:21:22 -0500

    todas las validaciones subidas DA

--- Complements/Validations.java
+++ Complements/Validations.java
@@ -1,5 +1,172 @@
 package Complements;
 
 public class Validations {
+    public boolean validateIDType(String idType) {
+        if (!idType.matches("^[01]$")) {
+            // mensaje de ID no es valido
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateID(String cedula) {
+        if (cedula.equals(null) || cedula.equals("")) {
+            // mensaje de vacio
+            return false;
+        }
+        if (!cedula.matches("\\d+")) {
+            // mensaje de no son numeros
+            return false;
+        }
+        if (cedula.length() < 10 || cedula.length() > 10) {
+            // mensaje de cedula no valida por rango
+            return false;
+        }
+
+        int[] coefficients = { 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+        int total = 0;
+
+        try {
+            int verifier = Integer.parseInt(cedula.substring(9));
+            for (int i = 0; i < 9; i++) {
+                int digito = Integer.parseInt(cedula.substring(i, i + 1));
+                int producto = digito * coefficients[i];
+                if (producto >= 10) {
+                    producto -= 9;
+                }
+                total += producto;
+            }
+
+            int residuo = total % 10;
+            if ((residuo == 0 && verifier == 0) || (residuo == 10 - verifier)) {
+
+                return true;
+            }
+        } catch (NumberFormatException e) {
+            // mensaje de error
+            return false;
+        }
+        // cedula no valida
+        return false;
+    }
+
+    public boolean validateRUC(String ruc) {
+        if(ruc.equals(null)|| ruc.equals("")){
+            //ruc vacio
+            return false;
+        }
+        if (!ruc.matches("\\d+")) {
+            // mensaje de no son numeros
+            System.out.println("solo numeros");
+            return false;
+        }
+        if ((ruc.length() != 10 && ruc.length() != 13)) {
+            // ruc no valido
+            return false;
+        }
+
+        // Validación para personas naturales
+        if (ruc.length() == 10) {
+            try {
+                int verificador = Integer.parseInt(ruc.substring(9));
+                int suma = 0;
+                for (int i = 0; i < 9; i++) {
+                    int digito = Integer.parseInt(ruc.substring(i, i + 1));
+                    suma += digito * (2 + (i % 7));
+                }
+                int residuo = suma % 11;
+                int digitoVerificador = residuo == 0 ? 0 : 11 - residuo;
+
+                return digitoVerificador == verificador;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }else {
+            try {
+                Long.parseLong(ruc);
+                return true;
+            } catch (NumberFormatException e) {
+                //mensaje de error
+                return false;
+            }
+        }
+
+    }
+    
+    public boolean validateName(String name){
+        if (name.matches("^[A-Za-záéíóúÁÉÍÓÚñÑ]+(\\s[A-Za-záéíóúÁÉÍÓÚñÑ]+)?$")) {
+            return true;
+        }else {
+        //mensaje nombre no valido
+        }
+        return false;
+    }
+    public boolean validateLastName(String lastName){
+        if (lastName.matches("^[A-Za-záéíóúÁÉÍÓÚñÑ]+(\\s[A-Za-záéíóúÁÉÍÓÚñÑ]+)?$")) {
+            return true;
+        }else {
+        //mensaje apellido no valido
+        }
+        return false;
+    }
+    public boolean validateAddress(String address){
+        if (address.matches("^[A-Za-z0-9áéíóúÁÉÍÓÚñÑ\\s.,-]{2,}$")) {
+            System.out.println("siuu");
+            return true;
+        }else {
+        //mensaje apellido no valido
+        }
+        return false;
+    }
+    public boolean validatePhoneNumber(String number){
+        if (number.matches("^09\\d{8}$")) {
+            return true;
+        }else {
+        //mensaje celular no no valido
+        }
+        return false;
+    }
+    public boolean validateEmail(String email){
+        if (email.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$")) {
+            return true;
+        }else {
+        //mensaje correo no no valido
+        }
+        return false;
+    }
+    //////////////////////////validaciones productos
+    // permitir números, letras o una combinación de ambos en el identificador
+    public boolean validateIdentifier(String identifier) {
+        if (identifier.matches("^[A-Za-z0-9]+$")) {
+            return true;
+        } else {
+            //mensaje identificador prodcuto no no valido
+        }
+        return false;
+    }
+    public boolean validateProductName(String name) {
+        if (name.matches("^[A-Za-z\\s-]+$")) {
+            return true;
+        } else {
+            //mensaje nombre producto no no valido
+        }
+        return false;
+    }
+    public boolean validatePrice(String price) {
+        if (price.matches("^(\\d+(\\.\\d{1,2})?)$")) {
+            return true;
+        } else {
+            //mensaje price no valido
+        }
+        return false;
+    }
+    public boolean validateIVA(String iva) {
+        if (iva.matches("^[0-9]+$")) {
+            return true;
+        } else {
+            //mensaje price no valido
+        }
+        return false;
+    }
 
 }
