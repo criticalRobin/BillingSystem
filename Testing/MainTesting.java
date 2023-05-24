c346bfad0fcacb3a0ae3cb3a62cba7f09040fcae
HenryCortez <100394133+HenryCortez@users.noreply.github.com>
2023-05-24 12:21:43 -0500

    main

--- Testing/MainTesting.java
+++ Testing/MainTesting.java
@@ -1,9 +1,11 @@
 package Testing;
 
+import Complements.InitComponents;
 import Views.Menu;
 
 public class MainTesting {
     public static void main(String[] args) {
+        InitComponents.initComponents();
         Menu.principalMenu();
     }
 }
